7d375c88aedf87ae833f0cd0b1a20c24d9eb39e7
Nela <Nela@DESKTOP-DDNDCMA>
2024-06-16 17:23:04 +0200

    prueba desde el E595

---
+++ src/git/Prueba_GitHub.java
@@ -0,0 +1,17 @@
+package git;
+
+import java.util.Scanner;
+
+public class Prueba_GitHub {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Introduce tu nombre");
+		String nombre = scanner.nextLine();
+		System.out.println("Tu nombre es " + nombre);
+		
+
+	}
+
+}
